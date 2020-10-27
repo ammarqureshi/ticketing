75f72e39101260605f2652e7c49bc44c369b3bcd
ammar <user@Users-MacBook-Pro.local>
2020-10-27 15:43:45 +0000

    startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,17 +3,18 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  console.log('starting up...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-if (!process.env.MONGO_URI){
-  throw new Error('MONGO_URI must be defined')
-}
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
     console.log('Connected to MongoDb');
   } catch (err) {
