07f3ce327ef374539c9e2accb588bcc166cf83bc
ammar <user@Users-MacBook-Pro.local>
2020-10-27 21:01:47 +0000

    startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('starting up auth service...');
+  console.log('starting up auth service....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
